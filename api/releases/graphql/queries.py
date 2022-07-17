e0f940eb09b35b426050ad7be221bc9ad4632afb
Cheikh Gueye Wane <cheikhgueye.wane@gmail.com>
2022-07-17 14:25:41 +0000

    fix typo name on get release

--- api/releases/graphql/queries.py
+++ api/releases/graphql/queries.py
@@ -5,10 +5,10 @@ from ..models import Release
 
 class Query(graphene.ObjectType):
     all_releases = graphene.List(ReleaseType)
-    release = graphene.Field(ReleaseType, release_id=graphene.Int())
+    release = graphene.Field(ReleaseType, id=graphene.ID(required=True))
 
     def resolve_all_releases(root, info):
         return Release.objects.all()
 
-    def resolve_releases(root, info, id):
+    def resolve_release(root, info, id):
         return Release.objects.get(pk=id)
