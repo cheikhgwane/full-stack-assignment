b8d655c70c0c442425afb8b6197253c28c274206
Cheikh Gueye Wane <cheikhgueye.wane@gmail.com>
2022-07-20 15:26:31 +0000

    check release form payload and force reload (#19)

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 export default function Header({ text, render }) {
   return (
     <div className="table_header">
-      <Link to="/">
-        <p style={{ marginTop: "10" }}>
-          All Releases <span style={{ textDecoration: "None" }}>{text && ">" + text}</span>
-        </p>
-      </Link>
+      <div>
+        <Link to="/">
+          <p style={{ marginTop: "10" }}>All Releases</p>
+        </Link>
+        <span>{text && ">" + text}</span>
+      </div>
+
       <div className="table_button">{render}</div>
     </div>
   );
